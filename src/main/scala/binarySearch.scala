35c47bd8bfc11c8ea8b6ccd516d3304a3135619c
Pritam Kadam <phkadam2008@gmail.com>
2020-10-02 20:51:22 +0530

    binary search

---
+++ src/main/scala/binarySearch.scala
@@ -0,0 +1,17 @@
+
+def search(arr: Array[Int], input: Int) = {
+    def bs(l: Int, r: Int): Int = {
+        val mid = (l + r) / 2
+        if (mid <= 0 || mid >= arr.length) -1
+        else if (arr(mid) == input) mid
+        else if (arr(mid) > input) bs(l, mid - 1)
+        else bs(mid + 1, r)
+    }
+
+    bs(0, arr.length)
+}
+
+@main
+def binarySearch = 
+    val input = Array(1, 5, 9, 11)
+    println(search(input, 9))
